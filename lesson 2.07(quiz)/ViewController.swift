463833f5ab84cd51f1d3465f93df5b13ee0fc90f
feeeeeen4ik <feeeeen4ik1994@gmail.com>
2024-11-07 20:37:07 +0500

    Initial Commit

---
+++ lesson 2.07(quiz)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson 2.07(quiz)
+//
+//  Created by Феликс Антонович on 07.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
