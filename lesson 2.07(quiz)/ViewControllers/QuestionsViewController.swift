fbdb590c83cfff896b0f7cdb27d5369074a0996e
feeeeeen4ik <feeeeen4ik1994@gmail.com>
2024-11-08 10:26:38 +0500

    end of lesson 2.07

--- lesson 2.07(quiz)/ViewControllers/QuestionsViewController.swift
+++ lesson 2.07(quiz)/ViewControllers/QuestionsViewController.swift
@@ -7,8 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class QuestionsViewController: UIViewController {
+    
+    private let questions = Question.getQuestions()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
---
+++ lesson 2.07(quiz)/ViewControllers/ResultsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ResultsViewController.swift
+//  lesson 2.07(quiz)
+//
+//  Created by Феликс Антонович on 08.11.2024.
+//
+
+import UIKit
+
+final class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
